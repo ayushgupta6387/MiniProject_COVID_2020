fe9851c6f4366785dad74035c2206e720376016f
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-12 19:21:48 +0530

    render a signup ejs file on our local server

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+app.get("/", (req, res)=>{
+res.render("signup");
+});
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
