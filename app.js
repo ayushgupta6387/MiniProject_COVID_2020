f6c3627bdb11dc1745b0bbce3d4a63d47fd659a8
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-10-12 19:13:23 +0530

    using port to run our website locally

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
+});
