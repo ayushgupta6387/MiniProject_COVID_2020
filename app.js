7739d0602797846268770518c4af04b9cfdc0d5f
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-10-18 10:27:07 +0530

    Changes in render method for home, signup and sign in page

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
-res.render("signup");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 
 app.get("/sign", (req, res) => {
-    res.render("signin");
+  res.render("signin");
 });
 
-app.get("/home", (req, res) => {
-    res.render("home");
+app.get("/register", (req, res) => {
+  res.render("signup");
 });
+
 app.get("/covidLive", (req, res) => {
-    res.render("covidLive");
+  res.render("covidLive");
 });
+
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
-
 }
 
 app.listen(port, function() {
