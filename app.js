871303ef6ad9a49a31bf150bdb6af0b437cc0c20
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-10-18 10:38:30 +0530

    Add const for request

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+
+const request = require("request");
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,7 +20,49 @@ app.get("/register", (req, res) => {
   res.render("signup");
 });
 
-app.get("/covidLive", (req, res) => {
+app.get("/covidlive", (req, res) => {
+  const url = "https://api.covid19india.org/data.json";
+    request(url, (error, response, body) => {
+
+        // Error - Any possible error when
+        // request is made.
+
+        // Eesponse - HTTP response status codes
+        // indicate whether a specific HTTP
+        // request has been successfully completed
+
+        // body - response data
+
+        // 200 - successful response
+        if (!error && response.statusCode == 200) {
+
+            // The response data will be in string
+            // Convert it to Object.
+            body = JSON.parse(body);
+            // The data have lot of extra properties
+            // We will filter it
+            var data = [];
+            for (let i = 0; i < body.statewise.length; i++) {
+                data.push({
+                    "State": body.statewise[i].state,
+
+                    "Confirmed": body.statewise[i].confirmed,
+
+                    "Active": body.statewise[i].active,
+
+                    "Recovered": body.statewise[i].recovered,
+
+                    "Death": body.statewise[i].deaths
+                });
+            }
+
+            console.log("-----Total Cases in India "
+                + "and in each state-----");
+
+            // Format to table
+            console.table(data);
+        }
+    })
   res.render("covidLive");
 });
 
