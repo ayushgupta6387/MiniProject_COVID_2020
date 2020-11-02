4e76e21baee82cf29790104aad555a936d63f107
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-11-02 13:46:45 +0530

    created user Schema

--- models/User.js
+++ models/User.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        max: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
