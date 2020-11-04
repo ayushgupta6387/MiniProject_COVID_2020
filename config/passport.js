c2830d794d78d1a4b1cd9ef1d4fe11546706473b
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-11-04 22:24:01 +0530

    apply serialize and deserialize

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 
 // load user model
 const User = require('../models/User');
+const { isMatch } = require('lodash');
 
 module.exports = function(passport) {
     passport.use(
@@ -11,9 +12,32 @@ module.exports = function(passport) {
             // match user
             User.findOne({ email: email })
                 .then(user => {
+                    if(!user){
+                        // error user msg
+                        return done(null, false, { mesage: 'That email is not registered' })
+                    }
 
+                    // match password
+                    // decrypt password to check  (user.password - hashed password [user coming from db]) 
+                    bcrypt.compare(password, user.password, (err, isMatch) => {
+                        if(err) throw err;
+                        if(isMatch){
+                            return done(null, user);
+                        }else{
+                            return done(null, false, { message: 'Password incorrect' })
+                        }
+                    });
                 })
                 .catch(err => console.log(err));
         })
-    )
+    );
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+          done(err, user);
+        });
+      });
 }
