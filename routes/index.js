0861ffc3395515bf1e2ebbf62e2019ea00c6d196
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-11-22 16:54:27 +0530

    change router.get method

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const request = require("request");
 const router = express.Router();
 const { ensureAuthenticated } = require("../config/auth");
 
 // open home by defaults
-router.get("/", (req, res) => res.render("home"));
+router.get("/", (req, res) => {
+
+const url = "https://api.covid19api.com/summary";
+request(url, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        body = JSON.parse(body);
+
+        res.render("home", {
+          allData: body.Global,
+        })
+    }
+    })
+});
+
+
+
 router.get("/dashboard", ensureAuthenticated, (req, res) =>
   res.render("dashbord", {
     name: req.user.name,
