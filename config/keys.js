d334b5aeb1abec2050bd9a9b371ac2de00995e7e
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-02-18 19:58:34 +0530

    fix bugs

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,9 @@
 // dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
-dbPassword =
-  "mongodb+srv://ayush:ayush6387@react-node-aws.0yb0b.mongodb.net/<dbname>?retryWrites=true&w=majority";
-
+dbPassword = 
+  // "mongodb+srv://ayush:ayush6387@react-node-aws.0yb0b.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  // "mongodb+srv://ayush:ayush6387@react-node-aws.0yb0b.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  "mongodb://localhost:27017/covid";
 module.exports = {
   mongoURI: dbPassword,
 };
